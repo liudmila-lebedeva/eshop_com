4c289216bf0716317f227677e9876453c77fcc50
Liudmila Lebedeva <cherubina2014@gmail.com>
2021-12-21 00:21:13 +0100

    add to catr 3 video

--- js/eshop.js
+++ js/eshop.js
@@ -1,3 +1,7 @@
+let cart = {
+    
+};    // bascket
+
 $('document').ready(function() {   //запускает только после загрузки html
     loadGoods();
 });
@@ -9,13 +13,27 @@ function loadGoods() {  //выгружаю товары на страницы
         let out = '';
         for (let key in data){    //перебираем массив
             
-            out+='<div class="single-goods">'
+            out+='<div class="single-goods">';
             out+='<h3>'+data[key]['name']+'</h3>';
             out+='<p>Price: '+data[key]['price']+'</p>';
             out+='<img src="'+data[key].image+'">';
-            out+='<button>Buy me</button>';
+            out+='<button class="add-to-cart" data="'+key+'">Buy me</button>'; // data"'+key+' -- adding of product key - articul  
             out+='</div>';
         }
         $('#goods').html(out);
+        $('button.add-to-cart').on('click', addToCart);
     });
+    
+    
 }
+
+function addToCart() { //add product to the cart
+        let vendorCode = $(this).attr('data'); //this = the button which I click 
+        if (cart[vendorCode] != undefined) {  //if product is in the bascket
+            cart[vendorCode]++;
+        }else {
+            cart[vendorCode] = 1; // if it is not so it is one
+        }
+        
+        console.log(cart);
+    }
